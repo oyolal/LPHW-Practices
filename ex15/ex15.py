1ab0ec5f110af325be9ebc589fc8db55a69f20b3
Omer Faruk Yolal <omeryol42@gmail.com>
2016-10-18 17:13:23 -0500

    comment added

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -1,13 +1,15 @@
 from sys import argv
 
 script, filename = argv
-
+# opening the file for editing
 txt = open(filename)
 
 print "Here's your file %r:" % filename
+#printing text from that file
 print txt.read()
 
 print "Type the filename again:"
+
 file_again = raw_input("> ")
 
 txt_again = open(file_again)
