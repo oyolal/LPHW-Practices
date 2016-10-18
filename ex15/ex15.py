d1885d0e560f1d6a110c406d6ec2e8ce1ca0b7fb
Omer Faruk Yolal <omeryol42@gmail.com>
2016-10-18 17:13:57 -0500

    comments added

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -9,7 +9,7 @@ print "Here's your file %r:" % filename
 print txt.read()
 
 print "Type the filename again:"
-
+#getting file name from user
 file_again = raw_input("> ")
 
 txt_again = open(file_again)
