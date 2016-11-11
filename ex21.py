cc6e8ac648ced8c0d443694c04d9ad0ea9ddc53c
Omer Faruk Yolal <omeryol42@gmail.com>
2016-11-10 19:22:04 -0600

    comment added

--- ex21.py
+++ ex21.py
@@ -1,15 +1,16 @@
+###function that adds two given numbers
 def add(a, b):
     print "ADDING %d + %d" % (a, b)
     return a + b
-
+###function that subtracts two given numbers
 def subtract(a, b):
     print "SUBTRACTING %d - %d" % (a, b)
     return a - b
-
+###function that multiplies two given numbers
 def multiply(a, b):
     print "MULTIPLYING %d * %d" % (a, b)
     return a * b
-
+###function that divides two given numbers
 def divide(a, b):
     print "DIVIDING %d / %d" % (a, b)
     return a / b
@@ -20,7 +21,7 @@ age = add(30,5)
 height = subtract(78,4)
 weight = multiply(90,2)
 iq = divide(100,2)
-
+#printing all varibles
 print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 
 
